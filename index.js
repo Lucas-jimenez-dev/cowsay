62512b0fc9d30384d4ec9f6194270a29b00e0fc4
Lucas-jimenez-dev <93341287+Lucas-jimenez-dev@users.noreply.github.com>
2022-03-21 14:00:08 +0100

    Update index.js
    erreur corrigé pour name ==> campus

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ console.log(userInfo.name);
 let cowsay = require("cowsay");
 
 console.log(cowsay.say({
-    text : `Helllo im ${userInfo.name} je suis à la ${userInfo.name}`
+    text : `Helllo im ${userInfo.name} je suis à la ${userInfo.campus}`
 }))
 
